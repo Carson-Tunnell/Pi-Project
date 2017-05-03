c267db832b3d5676f423d7548fcaa85f923cdfa7
Carson-Tunnell <carsontunnell@hotmail.com>
2017-05-02 23:58:45 -0600

    Updated to work
    Added new libraries to have temp sensor.cc run properly

--- temp_sensor.h
+++ temp_sensor.h
@@ -1,4 +1,8 @@
 #include <wiringPi.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
 
 
 class temp_sensor
@@ -7,6 +11,8 @@ class temp_sensor
        temp_sensor(int=4);    // call once to set the pin for the temp sensor
        double get_temp();          //returns a double with the current temp
        double get_humid();         //returns a double with the current humidity
+       int* read_dht11(); // reads raw data from DHT_11
     private:
         int pin;
+        int dht11_dat[5];
 };
